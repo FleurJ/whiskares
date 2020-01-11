ec2e5c54d0f24c17f1b3616bb0d43cee6f099e39
Ines Hogstrom <ines.hogstrom@icloud.com>
2020-01-11 16:31:51 +0100

    Created Mission Model

---
+++ db/migrate/20200111151631_create_missions.rb
@@ -0,0 +1,14 @@
+class CreateMissions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :missions do |t|
+      t.text :candidates, array: true, default: []
+      t.string :city
+      t.references :user, foreign_key: true
+      t.integer :fee
+      t.string :start_date
+      t.string :end_date
+
+      t.timestamps
+    end
+  end
+end
