e3fb795dbb9c69f30c8f6dd23ce7011f22d1082a
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-01-11 16:51:51 +0100

    animal model ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_151631) do
+ActiveRecord::Schema.define(version: 2020_01_11_154848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.integer "dangerosity"
+    t.text "needs"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_animals_on_user_id"
+  end
+
   create_table "missions", force: :cascade do |t|
-    t.text "candidates", array: true
+    t.text "candidates", default: [], array: true
     t.string "city"
     t.bigint "user_id"
     t.integer "fee"
@@ -39,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_01_11_151631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "animals", "users"
   add_foreign_key "missions", "users"
 end
