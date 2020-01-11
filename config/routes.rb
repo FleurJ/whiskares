5e2ccc8f9e5311a8651a8422691ac2ac6aaa9554
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-01-11 15:57:08 +0100

    small correction of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'homepage#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[show new edit create destroy] do
-    resources :animals, only: %i[show new create edit update destroy]
-    resources :missions, only: %i[index show new create edit update destroy]
+    resources :animals, only: %i[show new create edit update destroy] do
+      resources :missions, only: %i[index show new create edit update destroy]
+    end
   end
 end
