46361b0b6148f27e6da71f7e358537493a7b882b
Ines Hogstrom <ines.hogstrom@icloud.com>
2020-01-14 21:06:58 +0100

    Started working on users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def show
+    check_user
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def check_user
+    @user = User.find(params[:id])
   end
 end
