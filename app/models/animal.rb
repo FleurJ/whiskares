ecf44206875db6c2453fbdd6782f504db1f604aa
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-01-14 19:55:17 +0100

    validations ok

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,7 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :needs, presence: true
+  validates :dangerosity, inclusion: { in: %w[1 2 3 4 5] }
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,8 @@
 class Mission < ApplicationRecord
   belongs_to :user
+  validates :city, presence: true
+  validates :fee, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :animal_id, presence: true
 end
