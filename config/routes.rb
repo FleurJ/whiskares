6019abe8343746890c182362a7ec8010f6f05bb2
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-01-14 20:23:57 +0100

    restore routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'animals/show'
-  get 'animals/new'
-  get 'animals/create'
-  get 'animals/edit'
-  get 'animals/update'
-  get 'animals/destroy'
   devise_for :users
   root 'homepage#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
