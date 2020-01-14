7a0b85133fc96ea50a19c045d40aebf5450f7793
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-01-14 21:02:20 +0100

    Animal#show + view show

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,19 +1,37 @@
 class AnimalsController < ApplicationController
   def show
+    @animal = Animal.find(params[:id])
   end
 
-  def new
-  end
+  # def new
+  #   @animal = Animal.new
+  # end
 
-  def create
-  end
+  # def create
+  #   @animal = Animal.create(animal_params)
+  # end
 
-  def edit
-  end
+  # def edit
+  #   @animal = Animal.find(params[:animal_id])
+  # end
 
-  def update
-  end
+  # def update
+  #   @animal = Animal.find(params[:animal_id])
+  #   @animal.update(animal_params)
 
-  def destroy
+  #   redirect_to user_animal_path(@animal.user, @animal)
+  # end
+
+  # def destroy
+  #   @animal = Animal.find(params[:animal_id])
+  #   @animal.destroy
+
+  #   redirect_to user_animals_path(@animal.user)
+  # end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :dangerosity, :needs, :user)
   end
 end
