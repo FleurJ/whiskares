eecc9506e291346f205a85045edd734893f2ad8d
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-01-15 20:49:23 +0100

    seeds animals & users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.new(mail: "fleur.jasoigne@gmail.com", encrypted_password: "123456")
+User.new(mail: "fleur.jasoigne+1@gmail.com", encrypted_password: "123456")
+User.new(mail: "fleur.jasoigne+2@gmail.com", encrypted_password: "123456")
+
+Animal.new(name: "Merlin", species: "Wombat", dangerosity: "0", needs: "love")
+Animal.new(name: "Crumble", species: "persian cat", dangerosity: "0", needs: "frolics and hugs")
+Animal.new(name: "Charlie", species: "Unicorn", dangerosity: "2", needs: "pink water")
