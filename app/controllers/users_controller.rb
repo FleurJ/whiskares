17e4858b3dc27ff32c35d313a04e6770b042799f
Ines Hogstrom <ines.hogstrom@icloud.com>
2020-01-16 19:43:05 +0100

    Made edit and show views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
-  def show
-    check_user
-  end
+  before_action :check_user, only: [:edit, :update, :show]
 
-  def new
+  def show
   end
 
   def edit
   end
 
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :name, :phone, :introduction)
   end
 
   def check_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[show new edit create destroy] do
+  resources :users, only: %i[show new edit create destroy update] do
     resources :animals, only: %i[show new create edit update destroy] do
       resources :missions, only: %i[index show new create edit update destroy]
     end
